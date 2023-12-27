3f216d748fe2be02bc39477bbcdb07cc4ceb662f
Ray Wach <wach.ray+git@gmail.com>
2023-12-26 20:12:04 -0800

    Add a sandbox to define the game board

---
+++ src/routes/gameboard/+page.svelte
@@ -0,0 +1,34 @@
+<h1>Sample game board</h1>
+<div id="gameboard-div">
+  {#each Array(8).fill(0) as _}
+    <div class="grid-cell-div"/>
+  {/each}
+</div>
+
+<style>
+  #gameboard-div {
+    align-self: center;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-content: space-around;
+    background-color: aliceblue;
+    min-height: max-content;
+    min-width: max-content;
+    height: 100%;
+    width: 100%;
+  }
+
+  .grid-cell-div {
+    background-color: darkcyan;
+    min-height: 100px;
+    min-width: 100px;
+    margin: 2%;
+  }
+
+  .grid-cell-div:hover {
+    border-color: red;
+    border-style: solid;
+  }
+</style>
